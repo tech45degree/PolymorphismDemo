71f6da4599ec9570efb270708519949d39e5bee1
Muhammad Faisal <faisal.khan16@hotmail.com>
2023-06-19 00:30:22 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Shape shape = new Shape();
+        shape.draw();
+
+        Circle circle = new Circle();
+        circle.draw();
+
+        Square square = new Square();
+        square.draw();
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,7 @@
+public class Square extends Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Square is drawing");
+    }
+}
